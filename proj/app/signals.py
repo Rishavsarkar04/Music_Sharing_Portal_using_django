7133c7e506a12ef882231271ed630f75f5106031
Rishav Sarkar <85944172+Rishavsarkar04@users.noreply.github.com>
2023-06-18 01:05:13 +0530

    Update signals.py

--- proj/app/signals.py
+++ proj/app/signals.py
@@ -1,8 +1,18 @@
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from app.models import MusicFile
+from django.contrib.auth.signals import user_logged_out,user_logged_in
+from django.contrib import messages
 
 @receiver(pre_delete,sender=MusicFile)
 def files_delete(sender, instance, using, **kwargs):
     instance.file.delete()
     instance.image.delete()
+
+@receiver(user_logged_out)
+def on_user_logged_out(sender, request, **kwargs):
+    messages.add_message(request, messages.INFO, 'You Are Logged Out.')
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, request, **kwargs):
+    messages.add_message(request, messages.INFO, f'{request.user} Logged In.')
