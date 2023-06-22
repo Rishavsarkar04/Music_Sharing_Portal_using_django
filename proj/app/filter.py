b489ee3d2a501834982ed5de42dc58d10a27b298
Rishav Sarkar <85944172+Rishavsarkar04@users.noreply.github.com>
2023-06-23 00:30:14 +0530

    Update filter.py

--- proj/app/filter.py
+++ proj/app/filter.py
@@ -3,6 +3,13 @@ from app.models import MusicFile
 
 class MusicFilterForm(django_filters.FilterSet):
     uploader = django_filters.CharFilter(field_name='uploader__email',lookup_expr='icontains')
+    ordering = django_filters.OrderingFilter(choices=(('visibility', 'visibility sort'),
+                                                      ('title', 'title sort'),
+                                                      ),
+
+                                               fields=(('visibility', 'visibility'),
+                                                       ('title', 'title'),
+                                                       ),)
     class Meta:
         model = MusicFile
         fields = ['visibility']
